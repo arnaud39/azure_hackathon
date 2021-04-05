fe52aea125dffee3914b046be2d8a71251c748c9
limsiying <siying1899@gmail.com>
2021-04-05 15:03:40 +0800

    add more features

--- WeAreEngineers/pipeline/load_files.py
+++ WeAreEngineers/pipeline/load_files.py
@@ -12,5 +12,8 @@ def load_data() -> dict:
     data["sani_df"] = pd.read_csv(
         DIR + '/atLeastBasicSanitizationServices.csv')
     data["water_df"] = pd.read_csv(DIR + '/basicDrinkingWaterServices.csv')
-
+    data["fueltech_df"] = pd.read_csv(DIR + '/cleanFuelAndTech.csv')
+    data["gdi_df"] = pd.read_csv(DIR + '/gender_development.csv')
+    data["genderequal_df"] = pd.read_csv(DIR + '/gender_inequality.csv')
+    data["hdi_df"] = pd.read_csv(DIR + '/human_development.csv')
     return data
